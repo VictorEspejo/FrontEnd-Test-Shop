d6538574cd8433f7377ad20c84c549090dc04880
VictorEspejo <vicespejo96@gmail.com>
2022-04-06 20:06:09 +0200

    feat: relocate project files

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,12 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import ProductListPage from "./pages/ProductListPage";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <AppBar position="sticky" color="primary">
-          <Toolbar>
-            <Typography variant="h6">Navigation</Typography>
-          </Toolbar>
-        </AppBar>
         <Container maxWidth="lg">
           <Routes>
             <Route path="/" element={<ProductListPage />} />
